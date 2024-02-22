a3b00c2269012c1e82504b1a4babf83b336d122b
mizutanimeen <mizutanimeen@gmail.com>
2024-02-22 19:47:44 +0900

    Add 100vdh lib

--- client/app/src/App.tsx
+++ client/app/src/App.tsx
@@ -1,10 +1,12 @@
-import './App.css';
 import { Calendar } from './components/calendar/calendar';
+import Div100vh from 'react-div-100vh';
 
 export default function App() {
   return (
     <>
-      <Calendar />
+      <Div100vh>
+        <Calendar />
+      </Div100vh>
     </>
   );
 }
